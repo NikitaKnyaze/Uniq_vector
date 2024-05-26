9ec310791bda5d048e04d95acd9f48719653cbc1
Knyazev Nikita <nk8222138@gmail.com>
2024-05-26 17:49:08 +0300

    first commit

---
+++ Uniq_vector/Uniq_vector.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+   std::vector<int> my_v{ 1,1,2,5,6,1,2,4 };
+   std::sort(my_v.begin(), my_v.end());
+
+   for (const auto& s : my_v)
+      std::cout << s << " ";
+
+   std::cout << std::endl;
+   
+   auto it = std::unique(my_v.begin(), my_v.end());
+
+   my_v.erase(it, my_v.end());
+
+   for (const auto& s : my_v)
+      std::cout << s << " ";
+
+   return 0;
+}
+
